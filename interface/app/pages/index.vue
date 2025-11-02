f014e7173f29b3f91d6fc164c4d2796c5be83244
Colin-de-Seroux <77831261+Colin-de-Seroux@users.noreply.github.com>
2025-11-02 22:24:25 +0100

    :sparkles: Add rules (home page)

--- interface/app/pages/index.vue
+++ interface/app/pages/index.vue
@@ -2,8 +2,55 @@
 definePageMeta({
     layout: "dashboard",
 });
+
+import { useI18n } from "vue-i18n";
+
+const { tm } = useI18n();
+
+const rules = tm("home.rules") as {
+    title: string;
+    description: string[];
+}[];
 </script>
 
 <template>
-    <h1>{{ $t("home.title") }}</h1>
+    <div class="pa-6">
+        <h1 class="text-h4 font-weight-bold mb-8 text-center">
+            {{ $t("home.title") }}
+        </h1>
+
+        <div>
+            <section
+                v-for="(rule, i) in rules"
+                :key="i"
+                class="pa-6 rounded-lg mb-6 transition elevation-1 bg-homeSecondary"
+            >
+                <h2 class="text-h6 font-weight-bold d-flex align-center mb-3">
+                    <VIcon color="primary" class="mr-2">
+                        mdi-book-outline
+                    </VIcon>
+                    {{ $t(`home.rules.${i}.title`) }}
+                </h2>
+
+                <ul class="pl-8">
+                    <li
+                        v-for="(_, j) in rule.description"
+                        :key="j"
+                        class="text-body-2 d-flex align-start"
+                        :class="
+                            $t(`home.rules.${i}.description.${j}`).includes(
+                                '- ',
+                            )
+                                ? ''
+                                : 'mt-2'
+                        "
+                    >
+                        <span>
+                            {{ $t(`home.rules.${i}.description.${j}`) }}
+                        </span>
+                    </li>
+                </ul>
+            </section>
+        </div>
+    </div>
 </template>
--- interface/app/plugins/vuetify/theme.ts
+++ interface/app/plugins/vuetify/theme.ts
@@ -5,12 +5,13 @@ export const light: ThemeDefinition = {
     dark: false,
     colors: {
         primary: colors.blue.base,
-        secondary: colors.grey.base,
+        secondary: colors.blue.darken4,
         error: colors.red.base,
         info: colors.blue.base,
         success: colors.green.base,
         warning: colors.yellow.base,
         home: colors.shades.white,
+        homeSecondary: colors.grey.lighten3,
     },
     variables: {},
 };
@@ -19,12 +20,13 @@ export const dark: ThemeDefinition = {
     dark: true,
     colors: {
         primary: colors.blue.base,
-        secondary: colors.grey.base,
+        secondary: colors.blue.darken4,
         error: colors.red.base,
         info: colors.blue.base,
         success: colors.green.base,
         warning: colors.yellow.base,
         home: colors.shades.black,
+        homeSecondary: colors.grey.darken4,
     },
     variables: {},
 };
